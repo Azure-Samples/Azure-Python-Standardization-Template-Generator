3ee7ea98df314ea63dc82ad44ee57fb17626d686
Jay Miller <kjaymiller@gmail.com>
2024-01-29 10:55:59 -0500

    19-Add-OpenTelemetry-for-Django (#26)
    * Add OpenTelemetry for Django
    Fixes #19
    
    * adds conditional to configure_azure_monitor
    
    * update environment var
    
    * remove extra whitespace

--- {{cookiecutter.__src_folder_name}}/src/django/project/settings.py
+++ {{cookiecutter.__src_folder_name}}/src/django/project/settings.py
@@ -69,9 +69,6 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-if prod:
-    MIDDLEWARE.append("opencensus.ext.django.middleware.OpencensusMiddleware")
-
 ROOT_URLCONF = "project.urls"
 
 TEMPLATES = [
@@ -93,16 +90,6 @@ TEMPLATES = [
 WSGI_APPLICATION = "project.wsgi.application"
 
 
-OPENCENSUS = {
-    "TRACE": {
-        "SAMPLER": "opencensus.trace.samplers.ProbabilitySampler(rate=1)",
-        "EXPORTER": f"""opencensus.ext.azure.trace_exporter.AzureExporter(
-            connection_string="{os.environ.get('APPLICATIONINSIGHTS_CONNECTION_STRING')}"
-        )""",
-    }
-}
-
-# Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 db_options = {}
